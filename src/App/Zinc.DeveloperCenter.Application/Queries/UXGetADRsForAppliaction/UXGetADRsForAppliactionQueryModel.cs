a3561fab0a9396f8d9a09cd5cf797b0e22dcd1fb
Scott Horton <wshorton@hotmail.com>
2022-07-12 14:38:06 -0500

    Fix copy paste bug

--- src/App/Zinc.DeveloperCenter.Application/Queries/UXGetADRsForAppliaction/UXGetADRsForAppliactionQueryModel.cs
+++ src/App/Zinc.DeveloperCenter.Application/Queries/UXGetADRsForAppliaction/UXGetADRsForAppliactionQueryModel.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Zinc.DeveloperCenter.Application.Queries.UXGetADRsForAppliaction
 {
     /// <summary>
@@ -14,31 +8,31 @@ namespace Zinc.DeveloperCenter.Application.Queries.UXGetADRsForAppliaction
         /// <summary>
         /// Gets the application name where the ADR is defined.
         /// </summary>
-        public string? ApplicationName { get; protected set; }
+        public string? ApplicationName { get; set; }
 
         /// <summary>
         /// Gets the application display name where the ADR is defined.
         /// </summary>
-        public string? ApplicationDisplayName { get; protected set; }
+        public string? ApplicationDisplayName { get; set; }
 
         /// <summary>
         /// Gets the ADR title.
         /// </summary>
-        public string? Title { get; protected set; }
+        public string? Title { get; set; }
 
         /// <summary>
         /// Gets the ADR number.
         /// </summary>
-        public string? Number { get; protected set; }
+        public string? Number { get; set; }
 
         /// <summary>
         /// Gets the ADR last updated date.
         /// </summary>
-        public string? LastUpdated { get; protected set; }
+        public string? LastUpdated { get; set; }
 
         /// <summary>
         /// Gets the ADR content.
         /// </summary>
-        public string? Content { get; protected set; }
+        public string? Content { get; set; }
     }
 }
