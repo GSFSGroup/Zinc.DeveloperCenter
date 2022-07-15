e3b00e2879c30aeedaad94c4c1e3bb7b5a7827bf
Scott Horton <wshorton@hotmail.com>
2022-07-15 16:47:23 -0500

    Let's try a new database with full text search abilities

---
+++ src/App/Zinc.DeveloperCenter.Data.Migrations/Migrations/Migration_2022071502_RebuildDatabase.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics.CodeAnalysis;
+using FluentMigrator;
+
+namespace Zinc.DeveloperCenter.Data.Migrations.Migrations
+{
+    /// <summary>
+    /// Rebuild the database as more has been learned.
+    /// </summary>
+    [Migration(2022071502)]
+    [SuppressMessage("Minor Code Smell", "S101:Types should be named in PascalCase", Justification = "Migrations are named according to our standards.")]
+    public class Migration_2022071502_RebuildDatabase : ForwardOnlyMigration
+    {
+        private static string schemaName = "developercenter";
+        private static string tableName = "architecture_decision_record";
+        private static string contentTableName = "architecture_decision_record_content";
+
+        /// <inheritdoc/>
+        public override void Up()
+        {
+            Delete.Table(tableName).InSchema(schemaName);
+
+            Create
+                .Table(tableName)
+                .InSchema(schemaName)
+                .WithColumn("id").AsGuid().NotNullable().PrimaryKey($"{tableName}_pk")
+                .WithColumn("element").AsAnsiString().NotNullable()
+                .WithColumn("application_name").AsAnsiString().NotNullable()
+                .WithColumn("application_display_name").AsAnsiString().NotNullable()
+                .WithColumn("title").AsAnsiString().NotNullable()
+                .WithColumn("number").AsAnsiString().NotNullable()
+                .WithColumn("content_url").AsAnsiString().NotNullable()
+                .WithColumn("last_updated").AsAnsiString().Nullable()
+                ;
+
+            Create
+                .UniqueConstraint($"{tableName}_key")
+                .OnTable(tableName).WithSchema(schemaName)
+                .Columns("application_name", "number")
+                ;
+
+            Create
+                .Table(contentTableName)
+                .InSchema(schemaName)
+                .WithColumn("id").AsGuid().NotNullable().PrimaryKey($"{contentTableName}_pk")
+                .WithColumn("content").AsAnsiString().Nullable()
+                ;
+
+            Execute.Sql($"ALTER TABLE {schemaName}.{contentTableName} ADD COLUMN content_search tsvector GENERATED ALWAYS AS(to_tsvector('english', content)) STORED;");
+
+            Execute.Sql($"CREATE INDEX {contentTableName}_search_idx ON {schemaName}.{contentTableName} USING GIN (content_search);");
+        }
+    }
+}
