8421a0a079614b13e9d700c28d65111638a02ac0
Scott Horton <wshorton@hotmail.com>
2022-07-20 13:22:06 -0500

    Add missing using

--- src/App/Zinc.DeveloperCenter.Application/Queries/UXAppList/GetArchitectureDecisionRecords/UXAppListGetArchitectureDecisionRecordsQueryHandler.cs
+++ src/App/Zinc.DeveloperCenter.Application/Queries/UXAppList/GetArchitectureDecisionRecords/UXAppListGetArchitectureDecisionRecordsQueryHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using MediatR;
 using RedLine.Domain.Repositories;
 using Zinc.DeveloperCenter.Data.DataQueries;
