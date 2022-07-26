c3daf11bd645bd8631cb6f55aaddc75b14808ce3
Scott Horton <wshorton@hotmail.com>
2022-07-26 00:33:52 -0500

    Fix sql

--- src/App/Zinc.DeveloperCenter.Data/Repositories/ArchitectureDecisionRecordRepository.cs
+++ src/App/Zinc.DeveloperCenter.Data/Repositories/ArchitectureDecisionRecordRepository.cs
@@ -125,7 +125,7 @@ SELECT EXISTS (
 SELECT adr.*, COALESCE(views.view_count, 0) AS total_views
 FROM {TableName} AS adr
 LEFT OUTER JOIN {TableName}_viewcount AS views
-    ON views.id = adr.id AND
+    ON views.id = adr.id
 WHERE adr.tenant_id = @tenantId
 AND COALESCE(views.view_count, 0) > 0
 ORDER BY COALESCE(views.view_count, 0) DESC
