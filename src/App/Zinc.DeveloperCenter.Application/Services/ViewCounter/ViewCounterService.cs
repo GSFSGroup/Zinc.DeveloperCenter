83aa572dfb36f9cc3a241bd0d9e5ae60a57636a7
Scott Horton <wshorton@hotmail.com>
2022-07-25 21:44:51 -0500

    Adjust sql

--- src/App/Zinc.DeveloperCenter.Application/Services/ViewCounter/ViewCounterService.cs
+++ src/App/Zinc.DeveloperCenter.Application/Services/ViewCounter/ViewCounterService.cs
@@ -72,8 +72,8 @@ INSERT INTO developercenter.architecture_decision_record_viewcount (
     1
 )
 ON CONFLICT (id) DO
-UPDATE SET view_count = view_count + 1
-RETURNING view_count;";
+UPDATE SET architecture_decision_record_viewcount.view_count = architecture_decision_record_viewcount.view_count + 1
+RETURNING architecture_decision_record_viewcount.view_count;";
 
             public static readonly string UpdateViewCount = @"
 UPDATE developercenter.architecture_decision_record_viewcount
