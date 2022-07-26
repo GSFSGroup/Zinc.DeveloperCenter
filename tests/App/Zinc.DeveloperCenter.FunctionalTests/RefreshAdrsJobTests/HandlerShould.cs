1ed5dbc220697aec98b7b7f673c3db66d1996558
Scott Horton <wshorton@hotmail.com>
2022-07-26 13:11:13 -0500

    Attempting a long running functional test

---
+++ tests/App/Zinc.DeveloperCenter.FunctionalTests/RefreshAdrsJobTests/HandlerShould.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using RedLine.Application.Jobs;
+using Xunit;
+using Xunit.Abstractions;
+using Zinc.DeveloperCenter.Application.Jobs.RefreshAdrs;
+using Zinc.DeveloperCenter.Domain.Repositories;
+using Zinc.DeveloperCenter.Domain.Services.GitHub;
+
+namespace Zinc.DeveloperCenter.FunctionalTests.RefreshAdrsJobTests
+{
+    public class HandlerShould : FunctionalTestBase
+    {
+        public HandlerShould(FunctionalTestFixture fixture, ITestOutputHelper output)
+            : base(fixture, output)
+        {
+        }
+
+        [Fact]
+        public async Task PopulateTheDatabase()
+        {
+            var job = new RefreshAdrsJob("GSFSGroup", Guid.NewGuid());
+
+            var handler = new RefreshAdrsJobHandler(
+                GetRequiredService<IGitHubApiService>(),
+                GetRequiredService<IApplicationRepository>(),
+                GetRequiredService<IArchitectureDecisionRecordRepository>(),
+                GetRequiredService<ILogger<RefreshAdrsJobHandler>>);
+
+            var response = await handler.Handle(job, CancellationToken.None).ConfigureAwait(false);
+            response.Should().Be(JobResult.OperationSucceeded);
+        }
+    }
+}
