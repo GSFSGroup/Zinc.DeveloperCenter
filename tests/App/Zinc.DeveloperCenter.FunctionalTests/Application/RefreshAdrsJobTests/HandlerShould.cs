2b423996e162346a6dd68b74f1a629be64135b2d
Scott Horton <wshorton@hotmail.com>
2022-07-27 12:04:46 -0500

    Combine two separate tests because the database gets deleted between them

--- tests/App/Zinc.DeveloperCenter.FunctionalTests/Application/RefreshAdrsJobTests/HandlerShould.cs
+++ tests/App/Zinc.DeveloperCenter.FunctionalTests/Application/RefreshAdrsJobTests/HandlerShould.cs
@@ -11,6 +11,7 @@ using RedLine.Application.Jobs;
 using Xunit;
 using Xunit.Abstractions;
 using Zinc.DeveloperCenter.Application.Jobs.RefreshAdrs;
+using Zinc.DeveloperCenter.Application.Jobs.RefreshAdrsLastUpdated;
 using Zinc.DeveloperCenter.Domain.Repositories;
 using Zinc.DeveloperCenter.Domain.Services.GitHub;
 #pragma warning restore S1128 // Unused "using" should be removed
@@ -24,19 +25,13 @@ namespace Zinc.DeveloperCenter.FunctionalTests.Application.RefreshAdrsJobTests
         {
         }
 
-        // [Fact]
         [Fact(Skip = "Long running test.")]
-        public async Task PopulateTheDatabase()
+        public async Task UpdateTheDatabaseAndLastUpdated()
         {
-            var job = new RefreshAdrsJob("GSFSGroup", Guid.NewGuid());
+            var tenantId = "GSFSGroup";
+            var correlationId = Guid.NewGuid();
 
-            var handler = new RefreshAdrsJobHandler(
-                GetRequiredService<IGitHubApiService>(),
-                GetRequiredService<IApplicationRepository>(),
-                GetRequiredService<IArchitectureDecisionRecordRepository>(),
-                GetRequiredService<ILogger<RefreshAdrsJobHandler>>());
-
-            var response = await handler.Handle(job, CancellationToken.None).ConfigureAwait(false);
+            var response = await RunRefreshAdrsJob(tenantId, correlationId).ConfigureAwait(true);
             response.Should().Be(JobResult.OperationSucceeded);
 
             var connection = GetRequiredService<IDbConnection>();
@@ -55,6 +50,42 @@ namespace Zinc.DeveloperCenter.FunctionalTests.Application.RefreshAdrsJobTests
 
             totalRepos.Should().BeGreaterThan(50);
             totalAdrs.Should().BeGreaterThan(20);
+
+            response = await RunRefreshAdrsLastUpdatedJob(tenantId, correlationId).ConfigureAwait(false);
+            response.Should().Be(JobResult.OperationSucceeded);
+
+            totalAdrs = await connection
+                .ExecuteScalarAsync<int>("select count(*) from developercenter.architecture_decision_record where updated_by is not null AND updated_on is not null;")
+                .ConfigureAwait(false);
+
+            Output.WriteLine($"!!!!!!!!!!{totalAdrs} ADRs were updated!!!!!!!!!!");
+
+            totalAdrs.Should().BeGreaterThan(20);
+        }
+
+        private async Task<JobResult> RunRefreshAdrsJob(string tenantId, Guid correlationId)
+        {
+            var job = new RefreshAdrsJob(tenantId, correlationId);
+
+            var handler = new RefreshAdrsJobHandler(
+                GetRequiredService<IGitHubApiService>(),
+                GetRequiredService<IApplicationRepository>(),
+                GetRequiredService<IArchitectureDecisionRecordRepository>(),
+                GetRequiredService<ILogger<RefreshAdrsJobHandler>>());
+
+            return await handler.Handle(job, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        private async Task<JobResult> RunRefreshAdrsLastUpdatedJob(string tenantId, Guid correlationId)
+        {
+            var job = new RefreshAdrsLastUpdatedJob(tenantId, correlationId);
+
+            var handler = new RefreshAdrsLastUpdatedJobHandler(
+                GetRequiredService<IGitHubApiService>(),
+                GetRequiredService<IArchitectureDecisionRecordRepository>(),
+                GetRequiredService<ILogger<RefreshAdrsLastUpdatedJobHandler>>());
+
+            return await handler.Handle(job, CancellationToken.None).ConfigureAwait(false);
         }
     }
 }
